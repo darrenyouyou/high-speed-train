d7733b07b00b9eab43d8b2c956f3e7a5aa9ef54b
Darren You <jy37559624@gmail.com>
2023-08-28 23:05:02 +0800

    fetch api

--- src/views/theme/colors/Colors.js
+++ src/views/theme/colors/Colors.js
@@ -1,90 +1,63 @@
-import PropTypes from 'prop-types'
-import React, { useEffect, useState, createRef } from 'react'
-import classNames from 'classnames'
-import { CRow, CCol, CCard, CCardHeader, CCardBody } from '@coreui/react'
-import { rgbToHex } from '@coreui/utils'
-import { DocsLink } from 'src/components'
-
-const ThemeView = () => {
-  const [color, setColor] = useState('rgb(255, 255, 255)')
-  const ref = createRef()
-
-  useEffect(() => {
-    const el = ref.current.parentNode.firstChild
-    const varColor = window.getComputedStyle(el).getPropertyValue('background-color')
-    setColor(varColor)
-  }, [ref])
-
-  return (
-    <table className="table w-100" ref={ref}>
-      <tbody>
-        <tr>
-          <td className="text-medium-emphasis">HEX:</td>
-          <td className="font-weight-bold">{rgbToHex(color)}</td>
-        </tr>
-        <tr>
-          <td className="text-medium-emphasis">RGB:</td>
-          <td className="font-weight-bold">{color}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-const ThemeColor = ({ className, children }) => {
-  const classes = classNames(className, 'theme-color w-75 rounded mb-3')
-  return (
-    <CCol xs={12} sm={6} md={4} xl={2} className="mb-4">
-      <div className={classes} style={{ paddingTop: '75%' }}></div>
-      {children}
-      <ThemeView />
-    </CCol>
-  )
-}
-
-ThemeColor.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-}
+import React, { useEffect, useState } from 'react'
+import { clientID, clientSecret } from './constants'
 
 const Colors = () => {
+  const [token, setToken] = useState('')
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`,
+    }
+
+    fetch(
+      'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token',
+      requestOptions,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setToken(data.access_token)
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (token) {
+      fetch(
+        'https://tdx.transportdata.tw/api/basic/v2/Rail/THSR/DailyTimetable/Today?%24top=30&%24format=JSON',
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+        .then((response) => response.json())
+        .then((jsonData) => {
+          setData(jsonData)
+        })
+        .catch((error) => {
+          console.error('Error:', error)
+        })
+    }
+  }, [token])
+
   return (
-    <>
-      <CCard className="mb-4">
-        <CCardHeader>
-          Theme colors
-          <DocsLink href="https://coreui.io/docs/utilities/colors/" />
-        </CCardHeader>
-        <CCardBody>
-          <CRow>
-            <ThemeColor className="bg-primary">
-              <h6>Brand Primary Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-secondary">
-              <h6>Brand Secondary Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-success">
-              <h6>Brand Success Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-danger">
-              <h6>Brand Danger Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-warning">
-              <h6>Brand Warning Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-info">
-              <h6>Brand Info Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-light">
-              <h6>Brand Light Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-dark">
-              <h6>Brand Dark Color</h6>
-            </ThemeColor>
-          </CRow>
-        </CCardBody>
-      </CCard>
-    </>
+    <div className="App">
+      <h1>Token: {token}</h1>
+      {data && (
+        <div>
+          <h2>API Data:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </div>
   )
 }
 
